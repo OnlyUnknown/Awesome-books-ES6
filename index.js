42ece6ea2a358ca4918e8748c552c2c1d3e1a754
Abdullah <abdullahkhrief@gmail.com>
2023-02-28 16:12:22 +0300

    Date has been added

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { contact, bookShelf, addNew } from './modules/Transisions.js';
 import UI from './modules/UI-Class.js';
+import { DateTime } from './modules/luxon.js';
+
+const dateElement = document.querySelector('.Date');
+
+const date = DateTime.now();
+dateElement.textContent = date.toLocaleString(DateTime.DATETIME_MED);
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
